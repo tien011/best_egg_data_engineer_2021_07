295f94c68b5154c0f2ae7b6de7d542b2332e96fc
Tien Nguyen <hellotienng@gmail.com>
2022-10-06 20:05:58 -0400

    added file

---
+++ main.py
@@ -0,0 +1,32 @@
+"""
+This is the entrypoint to the program. 'python main.py' will be executed and the 
+expected csv file should exist in ../data/destination/ after the execution is complete.
+"""
+
+from src.some_storage_library import SomeStorageLibrary as SSL
+
+
+if __name__ == '__main__':
+    """Entrypoint"""
+    print('Beginning the ETL process...')
+
+    cols_file = open('data/source/SOURCECOLUMNS.txt', 'r')
+    cols = cols_file.read().split('\n')
+    cols_file.close()
+
+    data_file = open('data/source/SOURCEDATA.txt', 'r')
+    data = data_file.read().replace('|', ',')
+    data_file.close()
+
+    ls_name = []
+    cols_sorted = sorted(cols, key = lambda x: int(x.split('|')[0]))
+    for item in cols_sorted:
+        ls_name.append(item.split('|')[1])
+
+    final_table = ','.join(ls_name) +'\n' + data
+    f = open('column_data.csv', 'w')
+    f.write(final_table)
+    f.close()
+    
+    SSL().load_csv('column_data.csv')
+
